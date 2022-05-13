e981450e2411839e7face8a918a95bd7e35f7e9b
Kathirvelram <54325720+Kathirvelram@users.noreply.github.com>
2022-05-13 15:46:17 +0530

    update 4

--- AUTH_GROUP_CHAT/AUTHENICATION CLIENT/Main.cpp
+++ AUTH_GROUP_CHAT/AUTHENICATION CLIENT/Main.cpp
@@ -1,126 +1,240 @@
-#include "TCPClient.h"
 #include <iostream>
+#include "TCPClient.h"
 #include <string>
-#include <thread>
-
+#include <sstream>
+#include<fstream>
+#include <WS2tcpip.h>
+#include<map>
+#include <exception>
+#include <filesystem>
+#pragma comment (lib, "ws2_32.lib")
+namespace fs = std::filesystem;
 using namespace std;
-
-
-//When we create the client, we do not want the thread to run & try to receive data from the server until 
-TCPClient::TCPClient()
+int user_details_check();
+void filetransfer();
+struct userdetails
 {
-	recvThreadRunning = false;
-}
-
-
-TCPClient::~TCPClient()
+	string user_name;
+	string password;
+};
+void filetransfer()
 {
-	closesocket(serverSocket);
-	WSACleanup();
-	if (recvThreadRunning) {
-		recvThreadRunning = false;
-		recvThread.join();	//Destroy safely to thread. 
-	}
-}
+	fs::path sourceFile = "C:\\Users\\karthik\\SOCKET\\AUTH_GROUP_CHAT\\AUTHENICATION CLIENT\\user_check.txt";
+	fs::path targetParent = "C:\\Users\\karthik\\SOCKET\\AUTH_GROUP_CHAT\\AUTHENTICATION GROUP CHAT";
+	auto target = targetParent / sourceFile.filename(); // sourceFile.filename() returns "sourceFile.ext".
 
-
-bool TCPClient::initWinsock() {
-
-	WSADATA data;
-	WORD ver = MAKEWORD(2, 2);
-	int wsResult = WSAStartup(ver, &data);
-	if (wsResult != 0) {
-		cout << "Error: can't start Winsock." << endl;
-		return false;
-	}
-	return true;
-}
-
-SOCKET TCPClient::createSocket() {
-
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == INVALID_SOCKET) {
-		cout << "Error: can't create socket." << endl;
-		WSACleanup();
-		return -1;
-	}
-
-	//Specify data for hint structure. 
-	hint.sin_family = AF_INET;
-	hint.sin_port = htons(serverPort);
-	inet_pton(AF_INET, serverIP.c_str(), &hint.sin_addr);
-
-	return sock;
-
-}
-
-void TCPClient::threadRecv() {
-
-	recvThreadRunning = true;
-	while (recvThreadRunning) {
-
-		char buf[4096];
-		ZeroMemory(buf, 4096);
-
-		int bytesReceived = recv(serverSocket, buf, 4096, 0);
-		if (bytesReceived > 0) {			//If client disconnects, bytesReceived = 0; if error, bytesReceived = -1;
-
-			std::cout << string(buf, 0, bytesReceived) << std::endl;
-
-		}
-		if (bytesReceived == 0)
-		{
-			const char *buff = buf + '@';
-			send(serverSocket, buff, 4096, 0);
-		}
-
-	}
-}
-
-void TCPClient::connectSock(std::string usernamee ) {
-
-	//If !initWinsock -> return false. 
-
-	serverSocket = createSocket();
-	const char* username = usernamee.c_str();
-	cout << username << "mm"<<endl;
-	int connResult = connect(serverSocket, (sockaddr*)&hint, sizeof(hint));
-	if (connResult == SOCKET_ERROR) {
-		cout << "Error: can't connect to server." << endl;
-		closesocket(serverSocket);
-		WSACleanup();
-		return;
-	}
-	else
+	try // If you want to avoid exception handling, then use the error code overload of the following functions.
 	{
-		int a=send(serverSocket, username, strlen(username), 0);
-		cout << "username inside send " << username << endl;
-		if (a <0)
+		fs::create_directories(targetParent); // Recursively create target directory if not existing.
+		fs::copy_file(sourceFile, target, fs::copy_options::overwrite_existing);
+	}
+	catch (std::exception& e) // Not using fs::filesystem_error since std::bad_alloc can throw too.  
+	{
+		std::cout << e.what();
+	}
+}
+
+int user_details_check(string username,string userpassword)
+{
+	// filestream variable file
+	fstream file;
+	string word, t, q, filename;
+
+	// filename of the file
+	filename = "user_check.txt";
+	file.open(filename.c_str());
+	// opening file
+
+	
+	string user=username;
+	string pass=userpassword;
+	string temp;
+	int tempi;
+	int flag = 0;
+	// extracting words from the file
+	
+	while (file >> word)
+	{
+		//cout << "hello" << endl;
+		// displaying content
+		//cout << word << endl;
+		temp = word;
+		user = "";
+		pass = "";
+		//cout << temp<<endl;
+		for (int i = 0; i < temp.length(); i++)
 		{
-			cout << "username not send" << endl;
+
+			if (temp[i] != '_')
+			{
+				user = user + temp[i];
+			}
+			else if (temp[i] == '_')
+			{
+				tempi = i + 1;
+				while (tempi < temp.length())
+				{
+					pass = pass + temp[tempi];
+					tempi++;
+				}
+				break;
+			}
 		}
-		else if(a==0)
+		//cout << username<<endl;
+		//cout << user <<endl;
+		//cout << pass << endl;
+		int usercheck = user.compare(username);
+		int passcheck = pass.compare(userpassword);
+		//cout << usercheck << " " << passcheck << endl;
+		if (usercheck ==0&& passcheck==0)
 		{
-			cout << "connection closed" << endl;
+			
+			flag=1;
+			break;
 		}
-		else if (a > 0)
+		
+	}
+	if (flag == 1)
+	{
+		return 1;
+	}
+	else if(flag==0)
+	{
+		
+		return 0;
+	}				
+}
+void signup()
+{
+	ofstream file;
+	string username, password;
+	file.open("user_check.txt", std::ios_base::app);
+	s:
+		cout << "enter username:" << endl;
+
+		cin >> username;
+		cout << "enter password:" << endl;
+		cin >> password;
+		
+		int a = user_details_check(username,password);
+		if (a == 0)
 		{
-			cout << "send username" << endl;
+			file << username + "_";
+			file << password + ' ';
 		}
-		cout << "connected to the server" << endl;
+		else
+		{
+			cout << "username already exist" << endl;
+			goto s;
+		}
+		cout << "successfully signed up" << endl;
+		
+	
+}
+
+void login()
+{
+	int a = 0;
+	TCPClient* client = new TCPClient;
+	string msg = "a";
+	string username, password;
+	int flag = 0;
+	int c=0;
+	//cout << "loo" << endl;
+	while (true) {
+		cout << "enter username: " << endl;
+		cin >> username;
+		cout << "enter password: " << endl;
+		cin >> password;
+		a = user_details_check(username, password);
+		//cout << a << endl;
+		if (a == 0)
+		{
+			cout << "wrong credientials" << endl;
+			c++;
+			if (c == 2)
+			{
+				break;
+			}
+			else
+			{
+				continue;
+			}
+		}
+		if (a == 1)
+		{
+			cout << "successfully logged in" << endl;
+			flag = 1;
+			break;
+		}
+	}
+	if (flag == 1)
+	{
+		if (client->initWinsock()) {
+
+			client->connectSock(username);
+
+			client->recvThread = thread([&] {
+				client->threadRecv();
+				});
+		}
+
+
+		while (1)
+		{
+			getline(std::cin, msg);
+			std::string messageToSend;
+			client->username = username;
+			if (client->joinChat == false) {
+				std::ostringstream ss;
+				ss << msg;
+				messageToSend = ss.str();
+			}
+			else if (client->joinChat == true) {
+				//std::ostringstream ss;
+				//ss << client->username << " joined the chat!";
+				//messageToSend = ss.str();
+				client->joinChat = false;
+			}
+			client->sendMsg(messageToSend);
+			//a = 1;
+
+		}
 	}
 }
 
 
 
-void TCPClient::sendMsg(string txt) {
+int main() {
+	char user_choice,user_choice1;
+		
+		cout << "enter the choice : s (for signup), l (for login)" << endl;
+		cin >> user_choice;
+		switch (user_choice)
+		{
+		case 's':
+			signup();
+			filetransfer();
+			cout << "do u want to login (y/n):" << endl;
+			cin >> user_choice1;
+			if (user_choice1 == 'y')
+			{
+				login();
+				break;
+			}
+			else if (user_choice1== 'n')
+			{
+				break;
+			}
+		case 'l':
+			login();
+			break;
+		default:
+			cout << "enter the correct choice" << endl;
+			break;
 
-	if (!txt.empty() && serverSocket != INVALID_SOCKET) {
+		}
 
-		send(serverSocket, txt.c_str(), txt.size() + 1, 0);
-
-		//It wouldn't work with the previous version bc while we were constantly listening for received msgs, we would keep caling this fct. 
-		//This fct would send the message & try to handle the receiving too. It would get stuck while waiting for a received msg. 
-	}
+return 0;
 
 }
