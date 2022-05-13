2bc4a7240f3f1bb7041e6737a7c57be06cd93718
Kathirvelram <54325720+Kathirvelram@users.noreply.github.com>
2022-05-13 09:24:54 +0530

    update 3

--- AUTH_GROUP_CHAT/AUTHENTICATION GROUP CHAT/Main.cpp
+++ AUTH_GROUP_CHAT/AUTHENTICATION GROUP CHAT/Main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <string>
 #include "TCPServer.h"
-
+#include "linkedlist.h"
+#include"msg_add.h"
+#include<fstream>
 using namespace std;
-
 string serverIP = "127.0.0.1";
-
+class t
+{
+public:
+	void addmsg(std::string username, std::string msg)
+	{
+		ofstream file;
+		file.open("user_check.txt", std::ios_base::app);
+		file << username + "_";
+		file << msg + ' ';
+	}
+};
 int main() {
-
+	//ofstream file;
 	TCPServer server(serverIP, 54010);
 
 	if (server.initWinsock()) {
